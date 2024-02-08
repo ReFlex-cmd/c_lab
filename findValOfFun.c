0cd20870fb14b49786e05c5107aeed5934e4d22b
Комаров Дмитрий <dimakomarov29@gmail.com>
2024-02-09 00:27:56 +0300

    Update findValOfFun.c

--- findValOfFun.c
+++ findValOfFun.c
@@ -9,6 +9,6 @@ int main() {
     // скан а 
     for (float x_initial = -13.0; x_initial < x_final; x_initial += 2.5) {
         //проверка что знаменатель не равен нулю
-        
+        //bruh
     }
 } 
