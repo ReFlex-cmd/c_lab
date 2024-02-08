b5a0d955fbde69bb9045228fca6fec8daec7f01f
ReFlex-cmd <dimakomarov29@gmail.com>
2024-02-09 00:18:26 +0300

    first commit

---
+++ findValOfFun.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "math.h"
+
+int main() {
+    float x_final = 12.0;
+    float y;
+    float i;
+    //написать мол введите а 
+    // скан а 
+    for (float x_initial = -13.0; x_initial < x_final; x_initial += 2.5) {
+        //проверка что знаменатель не равен нулю
+        
+    }
+} 
