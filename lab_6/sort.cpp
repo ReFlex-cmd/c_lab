592a446029d80e3f2473d5c69019cefb22837e6b
ReFlex-cmd <dimakomarov29@gmail.com>
2024-03-06 22:36:48 +0300

    sort~

--- lab_6/sort.cpp
+++ lab_6/sort.cpp
@@ -6,10 +6,10 @@
 #include <Windows.h>
 
 void fillMatrix(std::vector<int>& matrix, int n) {
-    srand(time(NULL));  // Объявление рандома
+    srand(time(NULL));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matrix[i * n + j] = rand() % 100; // generates a random number between 0 and 99
+            matrix[i * n + j] = rand() % 100;
         }
     }
 }
@@ -17,8 +17,7 @@ void fillMatrix(std::vector<int>& matrix, int n) {
 void printMatrix(const std::vector<int>& matrix, int n) {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     srand(time(NULL));  // Объявление рандома
-    SetConsoleTextAttribute(h, rand() % 8); // Применяем рандом к цвету текста
-
+    SetConsoleTextAttribute(h, rand() % 8);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             std::cout << std::setw(3) << matrix[i * n + j];
@@ -28,6 +27,10 @@ void printMatrix(const std::vector<int>& matrix, int n) {
 }
 
 int main() {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO start_attribute;
+    GetConsoleScreenBufferInfo(h, &start_attribute);
+
     int n;
 
     std::cout << "Set N --> " << std::endl;
@@ -43,6 +46,8 @@ int main() {
     std::cout << "Original matrix: " << std::endl;
     fillMatrix(matrix, n);
     printMatrix(matrix, n);
+
+    SetConsoleTextAttribute(h, start_attribute.wAttributes);
 
     std::cout << "\nChanged matrix: " << std::endl;
 
